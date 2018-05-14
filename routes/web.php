c590115cfe5daba8ae62caf45ada0c97d0bac75c
Bino Oommen Samuel <binooommen37@gmail.com>
2018-05-14 01:45:30 -0400

    [UA-4] Code clean up

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ Route::get('/', 'UsersController@index');
 Route::get('/users', 'UsersController@index');
 Route::get('/users/create', 'UsersController@create');
 Route::get('/users/{user}', 'UsersController@show');
+//Using Get method for delete because of Form Method Spoofing limitations in laravel
+//We can pass a hidden method if needed
 Route::get('/users/delete/{id}', 'UsersController@delete');
 Route::post('/users', 'UsersController@store');
+//Using POST method for PUT because of Form Method Spoofing limitations in laravel
+//We can pass a hidden method if needed
 Route::post('/users/{id}', 'UsersController@update');
 
 // RESTful api's
