26ceed4917b48e874911c03ced6567c0a1d8e900
Bino Oommen Samuel <binooommen37@gmail.com>
2018-05-13 20:53:42 -0400

    [UA-1] Added user addresses, user role, user migrations, also added data for users

---
+++ database/migrations/2018_05_14_002310_create_user_addresses.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserAddresses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_addresses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->text('address');
+            $table->string('province');
+            $table->string('city');
+            $table->string('country');
+            $table->string('postal_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_addresses');
+    }
+}
